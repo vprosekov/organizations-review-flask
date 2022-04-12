615fe5b18786427e32ff2a6927fe035129a91a7d
Vladislav Prosekov <vprosekoff@gmail.com>
2022-04-13 05:32:16 +0600

    forheroku

--- app.py
+++ app.py
@@ -417,4 +417,4 @@ def create_article():
         return render_template('create-article.html')
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=3000, debug=True, threaded=True)
+    app.run()
